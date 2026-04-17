bb81ebc2f7e98467c6337cb9eeb704f317f1cd39
Baidik Deb <baidikmustnot@gmail.com>
2026-04-18 03:06:36 +0530

    Remove KolkataKomics, add UTF-8 env, better error logging

--- main.py
+++ main.py
@@ -178,4 +178,8 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        logger.exception(f"FATAL: {e}")
+        sys.exit(1)
